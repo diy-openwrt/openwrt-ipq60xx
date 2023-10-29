7f2ce4605dc545a65d16597532f0906a8899e05b
pexcn <pexcn97@gmail.com>
2023-10-29 18:14:28 +0800

    qualcommax: add Xiaomi AX1800 support
    references:
    - https://github.com/coolsnowwolf/openwrt-gl-ax1800/commit/0fc6174
    - https://forum.openwrt.org/t/adding-openwrt-support-for-netgear-rax120-nighthawk-ax12/75592/63

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -45,7 +45,6 @@ define Device/glinet_gl-axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
 
-
 define Device/jdc_ax1800-pro
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)
@@ -61,3 +60,16 @@ define Device/jdc_ax1800-pro
 	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$(BLOCKSIZE)
 endef
 TARGET_DEVICES += jdc_ax1800-pro
+
+define Device/xiaomi_ax1800
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX1800
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax1800
+endef
+TARGET_DEVICES += xiaomi_ax1800
